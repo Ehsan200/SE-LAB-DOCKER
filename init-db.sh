3c1eee030e5e6758c853be0fcb0e9658658794b3
moroukian <moroukianali@gmail.com>
2024-05-23 23:37:21 +0330

    feat: Remove Dockerfile.postgres & Use postgres:13 image directly

--- init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run custom SQL commands
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE example (id SERIAL PRIMARY KEY, data VARCHAR(100));
-EOSQL
